907a6b938356cdc5748abbb48b61f54cadd08677
Tobias Roth <tobias.roth.tr@gmail.com>
2019-09-30 14:21:27 +0200

    Taxi-v2: Find better alpha; yields alpha=0.25. 9.15 --> 9.27

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -2,7 +2,23 @@ from agent import Agent
 from monitor import interact
 import gym
 import numpy as np
+import sys
 
-env = gym.make('Taxi-v2')
-agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+best_alpha, best_reward = 0., float("-inf")
+
+for i in range(19):
+    alpha = 0.05 * i
+    print("\ni={}, alpha={}, best_reward={}".format(i, alpha, best_reward))
+
+    env = gym.make('Taxi-v2')
+    agent = Agent(alpha=alpha)
+    num_episodes = 20000
+    avg_rewards, best_avg_reward = interact(env, agent, num_episodes)
+    
+    if best_avg_reward > best_reward:
+        print("Updated best_reward with {}, alpha={}".format(best_avg_reward, alpha))
+        best_reward = best_avg_reward
+        best_alpha = alpha
+
+print('\n')
+print("Result:\nbest_alpha={}, best_reward={}".format(best_alpha, best_reward))
